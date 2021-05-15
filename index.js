b6abeb4ca9df2c2856512803d50ab252a86cbbb8
kurwitz3 <kurwitz3@aol.com>
2021-05-14 20:47:57 -0400

    commit tree 8c4c364c6533cf90525b3a3d759c95b881a254f8

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+
+
+class Polygon{
+    constructor(array){
+        this.array = array
+    }
+    get countSides(){
+        return this.array.length
+    }
+
+    get perimeter(){
+        return this.array.reduce((acc,value) => { return acc + value})
+    }
+}
+
+class Triangle{
+    
+}
